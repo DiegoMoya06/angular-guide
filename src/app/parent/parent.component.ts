ec3d37a011946c193418e758478a9e7beba90908
Diego Moya <diego0697ms@gmail.com>
2025-04-11 12:16:38 +0200

    Added multiple child components and communication

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,19 +1,31 @@
 import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ChildComponent} from '../child/child.component';
+import {UserCardComponent} from '../user-card/user-card.component';
+import {UserFormComponent} from '../user-form/user-form.component';
 
 @Component({
   selector: 'app-parent',
   standalone: true,
-  imports: [CommonModule, ChildComponent],
+  imports: [CommonModule, UserCardComponent, UserFormComponent, ChildComponent],
   templateUrl: './parent.component.html',
   styleUrl: './parent.component.css'
 })
 export class ParentComponent {
+  users = ['John', 'Alice'];
+  selectedUser: string | null = null;
   parentMessage = "Hi from Parent!";
   childReply = "";
 
-  handleChildMessage(message:string){
+  addUser(name: string) {
+    this.users.push(name);
+  }
+
+  onUserSelected(name: string) {
+    this.selectedUser = name;
+  }
+
+  handleChildMessage(message: string) {
     this.childReply = message;
   }
 }
